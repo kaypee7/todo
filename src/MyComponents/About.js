382e15181ef39d49b6017941d6e8e22ea4c30049
kaypee7 <87701272+kaypee7@users.noreply.github.com>
2021-07-30 12:51:52 +0530

    initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod voluptate at molestiae architecto, hic obcaecati laudantium assumenda maiores aliquam qui rem deserunt doloribus accusantium. Magni dolor tenetur incidunt exercitationem error eaque iste similique odit!</p>
+        </div>
+    )
+}
